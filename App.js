45c3381d7b17136023c6dcab01cb0c638cd14c79
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2021-12-10 12:37:56 +0530

    created a stack navigation

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <SafeAreaView style={{ flex: 1 }}>
+          <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+          </View>
+        </SafeAreaView>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
