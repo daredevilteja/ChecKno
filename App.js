66559846a898407fe27f39a38b5b574c208c2935
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2021-12-10 18:19:32 +0530

    Added Screens and navigation

--- App.js
+++ App.js
@@ -2,20 +2,49 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+function HomeScreen({ navigation }) {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text>This is the Home Screen!</Text>
+        <StatusBar style="auto" />
+      </View>
+      <Button
+        title="Go to CYK Screen"
+        onPress={() => navigation.navigate("Check Your Knowledge")}
+      />
+    </SafeAreaView>
+  );
+}
+
+function CYKScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text>This is the Check Your Knowledge Screen!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
+  );
+}
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <SafeAreaView style={{ flex: 1 }}>
-          <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
-          </View>
-        </SafeAreaView>
+      <Stack.Navigator
+        initialRouteName="Home"
+        // screenOptions={{ title: "ChecKno" }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Overview" }}
+        />
+        <Stack.Screen name="Check Your Knowledge" component={CYKScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -24,7 +53,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
