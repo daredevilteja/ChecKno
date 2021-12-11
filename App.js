8ed17ef145d6bc122e0c931518658b15662bb218
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2021-12-11 09:00:55 +0530

    Added some more navigation

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 function HomeScreen({ navigation }) {
   return (
@@ -10,21 +11,45 @@ function HomeScreen({ navigation }) {
       <View style={styles.container}>
         <Text>This is the Home Screen!</Text>
         <StatusBar style="auto" />
+        <Button
+          title="Profile"
+          onPress={() => navigation.navigate("Profile")}
+        />
+        <Button
+          title="Check Your Knowledge"
+          onPress={() => navigation.navigate("Check Your Knowledge")}
+        />
       </View>
-      <Button
-        title="Go to CYK Screen"
-        onPress={() => navigation.navigate("Check Your Knowledge")}
-      />
     </SafeAreaView>
   );
 }
 
-function CYKScreen() {
+function CYKScreen({ navigation, route }) {
+  const { itemId, otherParam } = route.params;
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
-        <Text>This is the Check Your Knowledge Screen!</Text>
+        <Text>This is not the Check Your Knowledge Screen!</Text>
+        <Text>itemId: {JSON.stringify(itemId)}</Text>
+        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
         <StatusBar style="auto" />
+        <Button
+          title="Go to CYK Screen"
+          onPress={() =>
+            navigation.push("Check Your Knowledge", {
+              itemId: Math.floor(Math.random() * 100),
+            })
+          }
+        />
+        <Button
+          title="Go to Home"
+          onPress={() => navigation.navigate("Home")}
+        />
+        <Button title="Go back" onPress={() => navigation.goBack()} />
+        <Button
+          title="Go back to first screen in stack"
+          onPress={() => navigation.popToTop()}
+        />
       </View>
     </SafeAreaView>
   );
@@ -34,19 +59,33 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        // screenOptions={{ title: "ChecKno" }}
-      >
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Overview" }}
-        />
-        <Stack.Screen name="Check Your Knowledge" component={CYKScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#f4511e",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        >
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: "Overview" }}
+          />
+          <Stack.Screen
+            name="Check Your Knowledge"
+            component={CYKScreen}
+            initialParams={{ itemId: 42 }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
